7822318eb4722b998074bf766a8199963bbcdbad
Evan Geer <rust@evangeer.com>
2023-02-24 00:43:54 -0700

    start

---
+++ src/components/MainMenu.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import menuButtons from "../types/menuButtons";
+import QuickCashButton from "./QuickCashButton";
+
+export function MainMenu({ balance }: { balance: number }) {
+  // export function MainMenu({onNav, balance}) {
+  // const handleClick = (e) => {
+
+  //     let nav = e.target.textContent;
+  //     console.log(nav);
+  //     onNav(nav);
+  // }
+
+  // const atLeast = (val) => {
+  //     return val >= balance ? true : '';
+  // }
+
+  return (
+    <>
+      Choose a Transaction
+      <div className="grid-container">
+        <Link className="col col1" to="/deposit">
+          {menuButtons.deposit}
+        </Link>
+
+        <div className="col col1">
+          <QuickCashButton amount={80} balance={balance} />
+        </div>
+
+        <div className="col1">&nbsp;</div>
+
+        <Link className="col col1" to="/withdrawal">
+          {menuButtons.withdrawal}
+        </Link>
+        <Link className="col col1" to="/statement">
+          {menuButtons.statement}
+        </Link>
+        <Link className="col col1" to="/transfer">
+          {menuButtons.transfer}
+        </Link>
+      </div>
+    </>
+  );
+}
